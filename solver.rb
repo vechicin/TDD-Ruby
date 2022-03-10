4141ef60816b85e2195d58cc057a5a0b73e4fc35
Santiago Velosa <santiagovelosa@gmail.com>
2022-03-10 14:34:52 -0500

    Finish writing for factorial method

--- solver.rb
+++ solver.rb
@@ -1,6 +1,12 @@
 class Solver
   def factorial(number)
+    raise 'Number must be >= 0' if number < 0
 
+    raise 'Number must be an integer' unless number. is_a? Integer
+
+    return 1 if number == 0
+
+    return number * factorial(number - 1)
   end
 
   def reverse(word); end
--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -4,13 +4,13 @@ require_relative '../solver'
 describe Solver do
   describe '#factorial' do
     it 'should raise an error if number is negative' do
-      expect(Solver.new.factorial(-1)).to eq('Number must be >= 0')
+      expect{ Solver.new.factorial(-1) }.to raise_error('Number must be >= 0')
     end
 
     it 'should raise an error if number is not an integer' do
-      expect(Solver.new.factorial(10.4)).to eq('Number must be an integer')
+      expect{ Solver.new.factorial(10.4) }.to raise_error('Number must be an integer')
     end
-    
+
     it 'should return 1 if number is 0' do
       expect(Solver.new.factorial(0)).to eq(1)
     end
